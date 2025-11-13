7797d30140d89ea036b79f04f3e5f785bf07a05b
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-11-14 00:31:33 +0600

    added menu and main loop

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ class PolynomialEvaluator
 {
     public static void Main()
     {
-        
+        string userInput;
+        Console.WriteLine("Polynomial Calculator");
+        Console.WriteLine("Type the polynomial you want to calculate.");
+        Console.WriteLine("Example: 2x^2 + 3x + 8");
+        Console.WriteLine("'q' to exit.");
+
+        while (true)
+        {
+            userInput = Console.ReadLine();
+                
+            if (userInput == "q") break;
+            if (userInput == "") Console.WriteLine("Please enter the expression");
+        }
     }
 }
